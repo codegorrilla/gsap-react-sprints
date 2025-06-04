4e3368fcd5ed366da171eaeebd161e2371336088
Sanjib Adhya <code.gorrilla@gmail.com>
2025-06-04 11:25:37 +0530

    added third lesson

--- 03/src/App.jsx
+++ 03/src/App.jsx
@@ -4,6 +4,8 @@ import { useGSAP } from '@gsap/react';
 
 gsap.registerPlugin(useGSAP);
 
+const randomX = gsap.utils.random(-200, 200, 1, true);
+
 export default function App() {
 	return <></>;
 }
