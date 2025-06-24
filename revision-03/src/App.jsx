9c01ae2d212bdd6cde5f7b93d8cc3722ff06a345
Sanjib Adhya <sanjib@zacksresearch.in>
2025-06-24 18:34:42 +0530

    added demonstration for showing how to use contextSafe method of making sure of about the click evdent being disposed off while unmounting any component

---
+++ revision-03/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState, useRef } from 'react';
+import { gsap } from 'gsap';
+import { useGSAP } from '@gsap/react';
+
+export default function app() {
+	const container = useRef();
+	const badRef = useRef();
+	const goodRef = useRef();
+
+	useGSAP(
+		(context, contextSafe) => {
+			gsap.to(goodRef.current, { x: 100 });
+
+			//bad practice
+			badRef.current.addEventListener('click', () => {
+				gsap.to(badRef.current, { y: 100 });
+			});
+
+			//safe
+			const onClickGood = contextSafe(() => {
+				gsap.to(goodRef.current, { rotation: 180 });
+			});
+
+			goodRef.current.addEventListener('click', onClickGood);
+
+			//we remove the event listener in the cleanup function below
+			return () => {
+				goodRef.current.removeEventListener('click', onClickGood);
+			};
+		},
+		{ scope: container }
+	);
+
+	return (
+		<div ref={container}>
+			<button ref={badRef}>Bad</button>
+			<button ref={goodRef}>Good</button>
+		</div>
+	);
+}
